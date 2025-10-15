44d0c26e25b5fc686a8200391dccc57cb975a783
Aura Bintang Aprilian <auraaprilian0@gmail.com>
2025-10-15 14:01:56 +0700

    Percobaan While

---
+++ KafeDoWhile06.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class KafeDoWhile06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int kopi, teh, roti, totalHarga;
+        String namaPelanggan;
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+
+        do {
+           System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar) : ");
+              namaPelanggan = sc.nextLine();
+              if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break; 
+              }
+              System.out.print("Jumlah kopi\t\t: ");
+              kopi = sc.nextInt();
+              System.out.print("Jumlah teh\t\t: ");
+                teh = sc.nextInt();
+                System.out.print("Jumlah roti\t\t: ");
+                roti = sc.nextInt();
+              totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+                System.out.println("Total harga untuk " + namaPelanggan + " adalah : Rp " + totalHarga);
+                sc.nextLine();
+        } while (true);
+        
+        System.out.println("Semua transaksi selesai.");
+    }
+}
